291f65b944b56efc33928b96034936c4675e3fb0
gj-krsnv <gj-krsnv@ya.ru>
2024-01-11 00:58:12 +0300

    added blazor bootstrap. added table grid with sort and filters

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,7 +5,7 @@ using WeatherApp.Client;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddBootstrapBlazor();
+builder.Services.AddBlazorBootstrap();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 
--- Server/Program.cs
+++ Server/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddBlazorBootstrap();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
